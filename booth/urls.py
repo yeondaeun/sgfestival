d06698c664a4fc2f6d41ab6434aafa702919aaf6
babylionjane <runinging@gmail.com>
2019-05-13 04:45:42 +0900

    jane uploaded some images according to the days
    hey

--- booth/urls.py
+++ booth/urls.py
@@ -9,5 +9,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('boothindex/', views.booth_list, name='booth_list'),
     path('fleeindex/', views.flee_list, name='flee_list'),
+    path('tuesindex/', views.tues_list, name='tues_list'),
+    path('wednesindex/', views.wednes_list, name='wednes_list'),  
+    path('thursindex/', views.thurs_list, name='thurs_list'),  
     path('detail/<int:index>', views.booth_detail, name='booth_detail'),
 ]
