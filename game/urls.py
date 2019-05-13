6bd5b0f342f12fba0bc29837f3232fb0adbc1437
citrusj <37509450+citrusj@users.noreply.github.com>
2019-05-14 08:46:46 +0900

    retrieve quiz view

--- game/urls.py
+++ game/urls.py
@@ -8,5 +8,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('answer_new/',views.answer_new, name = "answer_new"),
     path('result/<pk>/', views.answer_result, name="game_result"),
-    path('start/',views.game_start, name="game_start")
+    path('start/',views.game_start, name="game_start"),
+    path('waiting/', views.game_waiting, name="game_waiting"),
 ]
--- game/views.py
+++ game/views.py
@@ -5,6 +5,8 @@ from .models import Question, Answer, Award
 def game_start(request):
     return render(request, "game/game_start.html")
 
+def game_waiting(request):
+    return render(request, "game/game_waiting.html")
 
 def answer_new(request):
     questions = Question.objects.all
@@ -30,17 +32,44 @@ def answer_new(request):
         a19 = request.POST.get('answer_19')
         a20 = request.POST.get('answer_20')
         name = request.POST.get('name')
+        
+        i1 = request.POST.get('id_1')
+        i2 = request.POST.get('id_2')
+        i3 = request.POST.get('id_3')
+        i4 = request.POST.get('id_4')
+        i5 = request.POST.get('id_5')
+        i6 = request.POST.get('id_6')
+        i7 = request.POST.get('id_7')
+        i8 = request.POST.get('id_8')
+        i9 = request.POST.get('id_9')
+        i10 = request.POST.get('id_10')
+        i11 = request.POST.get('id_11')
+        i12 = request.POST.get('id_12')
+        i13 = request.POST.get('id_13')
+        i14 = request.POST.get('id_14')
+        i15 = request.POST.get('id_15')
+        i16 = request.POST.get('id_16')
+        i17 = request.POST.get('id_17')
+        i18 = request.POST.get('id_18')
+        i19 = request.POST.get('id_19')
+        i20 = request.POST.get('id_20')
+        answerset = {i1:a1, i2:a2, i3:a3, i4:a4, i5:a5, i6:a6, i7:a7, i8:a8, i9:a9, i10:a10, 
+        i11:a11, i12:a12, i13:a13, i14:a14, i15:a15, i16:a16, i17:a17, i18:a18, i19:a19, i20:a20}
+        realanswerset = {}
 
-        answerset = [a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20]
-        realanswerset = []
-
-        for i in range(1, 21,1): 
-            question =Question.objects.get(id=i)
-            realanswerset.append(question.answer)
+        # for i in range(1, 21,1): 
+        #     question =Question.objects.get(id=i)
+        #     realanswerset.append(question.answer)
+        # for i in Question.objects.all:
+        #     realanswerset[i.id]=i.answer
+        for k in answerset.keys():
+            question = Question.objects.get(id=k)
+            realanswerset[k] = question.answer
         total=0
-        for i in range(20):
-            if int(realanswerset[i])==int(answerset[i]): total+=1
-
+        # for i in range(20):
+        #     if int(realanswerset[i])==int(answerset[i]): total+=1
+        for k, v in answerset.items():
+            if(realanswerset[k]==int(v)): total+=1
         answer = Answer.objects.create(c1=a1, c2=a2, c3=a3, c4=a4, c5=a5, c6=a6, c7=a7, c8=a8, c9=a9, c10=a10, c11=a11, c12=a12, c13=a13,
         c14=a14, c15=a15, c16=a16, c17=a17, c18=a18, c19=a19, c20=a20, name=name, total=total)
 
