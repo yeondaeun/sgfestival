e47f0b21a7fac17537dbb8e60357e75571d04522
paekjisoo <natuur714@naver.com>
2019-05-13 09:26:28 +0900

    gitignore이랑 settings.py배포용으로 수정

--- sgfestival/settings.py
+++ sgfestival/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j5(p#9!l_k)87$@35y(7yg)v9dk9f#i7@g)ej_xkjdto8xghwr'
+# SECRET_KEY = 'j5(p#9!l_k)87$@35y(7yg)v9dk9f#i7@g)ej_xkjdto8xghwr'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'j5(p#9!l_k)87$@35y(7yg)v9dk9f#i7@g)ej_xkjdto8xghwr')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['*']
 
